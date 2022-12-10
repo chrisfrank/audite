40a90a8474e9fdde04b6b654611e3f4cad57692b
Chris Frank <cgfrank@gmail.com>
2022-12-10 09:25:18 -0500

    Release to pypi on tag

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="audite",
-    version="0.4.4",
+    version="0.4.5",
     description="Instant data auditing for SQLite",
     long_description=long_description,
     long_description_content_type="text/markdown",
